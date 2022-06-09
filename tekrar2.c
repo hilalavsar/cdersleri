767da2971ae75734e27c67dc0093203bc8b81800
hilalavsar <hilalavsar22@gmail.com>
2022-06-09 13:43:19 +0300

    Update tekrar2.c

--- tekrar2.c
+++ tekrar2.c
@@ -2,14 +2,23 @@
 #include <stdlib.h>
 
 int main(){
-    int tamsayi; // 4 byte
-    double büyükyerkaplayankesirlisayi;// 8 byte
+    int tamsayi= 9;
+    printf("%d RAM'de %d byte yer kaplar.\n",tamsayi,sizeof(int));
+                                       // 4 byte
+    double buyukyerkaplayankesirlisayi= 5.7;           // 8 byte
+    printf("%.2f RAM'de %f byte yer kaplar.\n",buyukyerkaplayankesirlisayi,sizeof(double));
     /*short int uzunluk;//2 byte
     long int uzunluk2;//4 byte*/
-    char karakterdizisi;// 1 byte
-    char karakterdizisi[10]);// 10 byte
-    float kesirli sayi; // 4 byte
 
+    char karakter = 55;                       // 1 byte
+    printf("%c RAM'de %c byte yer kaplar.\n",karakter,sizeof(char));
+    //char karakterdizisi[14] = "HiLAL COMPANY" ; // 10 byte
+    float kesirlisayi= 2.2 ;                  // 4 byte
+    printf("%.4f RAM'de %f byte yer kaplar.\n",kesirlisayi,sizeof(float));
+
+    char karakterdizisi2[6]= "hilal";
+    printf("%s RAM'de %s byte yer kaplar.",karakterdizisi2,sizeof(char));
 
     return 0;
 }
+
